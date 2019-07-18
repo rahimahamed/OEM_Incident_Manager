e85367a271a6f7289b944ceb9368bc479b457b14
Sayef Iqbal <sayef20@gmail.com>
2019-07-18 15:17:07 -0400

    ENV

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,3 +1,6 @@
+if(process.env.NODE_ENV !== 'production'){
+  require('dotenv').config(); //not load
+}
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
