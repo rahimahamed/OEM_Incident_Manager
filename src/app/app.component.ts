64dcf5c0c005b87ec1e65bbde0d491bab24bd7ab
Saha <rsaha@OEM.NYCNET>
2019-07-23 16:02:45 -0400

    Working clock typescript

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  date: number = Date.now();
+  showTime(){
+    let date = new Date();
+    let time = "";
+
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+
+    time = h + ":" + m + ":" + s;
+    setTimeout(this.showTime, 1000)
+    return time;
+  }
+
+
+  /*time = "";
+  date = new Date();
+
+  show(){
+    let h = this.date.getHours();
+    let m = this.date.getMinutes();
+    let s = this.date.getSeconds();
+    if(h == 0){
+      h = 12;
+      this.time = h + ":" + m + ":" + s;
+    }
+    if(h > 12){
+      h = h - 12;
+      this.time = h + ":" + m + ":" + s;
+    }
+    if(h < 10){
+      this.time = "0" + h + ":" + m + ":" + s;
+    }
+    if(m < 10){
+      this.time = h + ":" + "0" + m + ":" + s;
+    }
+    if(s < 10){
+      this.time = h + ":" + m + ":" + "0" + s;
+    }
+
+    //var time = h + ":" + m + ":" + s;
+    console.log(this.time);
+    setTimeout(this.showTime, 1000);
+    return '';
+  }
+
+  //timer:  = this.showTime();
+
+  //date: number = Date.now();*/
 }
