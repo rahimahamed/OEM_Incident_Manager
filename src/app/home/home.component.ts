c7bd1a240e78faf3c7302f9400da85d1a1468b0a
Saha <rsaha@OEM.NYCNET>
2019-07-25 16:50:08 -0400

    updated

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,8 @@ import { IncidentService } from './../incident.service';
 import { Incident } from './../incident';
 import { Router, ActivatedRoute } from '@angular/router';
 import { IncidentsDataSource } from '../incident.data.source';
+import { FormGroup, FormControl } from '@angular/forms';
+import { IfStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-home',
@@ -10,11 +12,42 @@ import { IncidentsDataSource } from '../incident.data.source';
   styleUrls: ['./home.component.css'],
   providers: [IncidentService]
 })
+
 export class HomeComponent implements OnInit {
   title = 'NYC Emergency Incident Tracker';
   private hideForm = true;
   date: number = Date.now();
   dataSource: IncidentsDataSource;
+
+  check1= "false";
+  check2 = "false";
+  check3 = "false";
+
+
+
+  //selectedValue: string = "";
+  statuses = [
+    {value: 'Open', viewValue: 'Open'},
+    {value: 'Report Closed', viewValue: 'Report Closed'},
+    {value: 'Special Attention', viewValue: 'Special Attention'}
+  ];
+
+  prognoses1 = [
+    {value: 'Monitoring', viewValue: 'Monitoring'},
+    {value: 'Response', viewValue: 'Response'},
+    {value: 'Extended Operation', viewValue: 'Extended Operation'}
+  ]
+
+  prognoses2 = [
+    {value: 'Monitoring', viewValue: 'Monitoring'},
+    {value: 'Response', viewValue: 'Response'},
+  ]
+
+  prognoses3 = [
+    {value: 'Monitoring', viewValue: 'Monitoring'},
+  ]
+
+
 
   model = new Incident();
 
@@ -39,6 +72,23 @@ export class HomeComponent implements OnInit {
     );
   }
 
+  onSelect(){
+    if(this.model.STATUS == "Open"){
+      this.check1 = "true";
+      this.check2 = "false";
+      this.check3 = "false";
+    }
+    if(this.model.STATUS == "Report Closed"){
+      this.check1 = "false";
+      this.check2 = "true";
+      this.check3 = "false";
+    }
+    if(this.model.STATUS == "Special Attention"){
+      this.check1 = "false";
+      this.check2 = "false";
+      this.check3 = "true";
+    }
+  }
   incidentSelect(dataSource: IncidentsDataSource) {
     this.dataSource = dataSource;
   }
