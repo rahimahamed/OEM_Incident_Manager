497c4ed5a44ddf4818d06dafab1da85cd03c52e0
Saha <rsaha@OEM.NYCNET>
2019-07-29 17:01:10 -0400

    Update

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,6 +5,8 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { IncidentsDataSource } from '../incident.data.source';
 import { FormGroup, FormControl } from '@angular/forms';
 import { IfStmt } from '@angular/compiler';
+import { of } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -19,14 +21,72 @@ export class HomeComponent implements OnInit {
   date: number = Date.now();
   dataSource: IncidentsDataSource;
 
-  check1= "false";
-  check2 = "false";
-  check3 = "false";
+  public isDisabledPrognosis: boolean = true;
+
+  public defaultStatus: { statusName: string, statusId: number} = { statusName: "Select A Status", statusId: null};
+  public defaultPrognosis: { prognosisName: string, prognosisId: number} = { prognosisName: "Select a Prognosis", prognosisId: null};
+
+  public statusCategory: Array<{ statusName: string, statusId: number}> = [
+    {statusName: "Report Closed", statusId: 0},
+    {statusName: "Open", statusId: 1},
+    {statusName: "Special Attention", statusId: 2}
+  ];
+
+  public statusPrognosis: Array<{ prognosisName: string, prognosisId: number, statusId: number}> = [
+    {prognosisName: "Monitoring", prognosisId: 0, statusId: 0},
+    {prognosisName: "Response", prognosisId: 1, statusId: 0},
+    {prognosisName: "Monitoring", prognosisId: 2, statusId: 1},
+    {prognosisName: "Response", prognosisId: 3, statusId: 1},
+    {prognosisName: "Extended Operation", prognosisId: 4, statusId: 1},
+    {prognosisName: "Monitoring", prognosisId: 5, statusId: 2},
+  ]
+
+  public dataResultPrognosis: Array<{prognosisName: string, prognosisId: number, statusId: number}>;
+
+  public selectedStatus: {statusName: string, statusId: number};
+  public selectedPrognosis: {prognosisName: string, prognosisId: number};
+
+  handleStatusChange(value){
+    this.selectedStatus = value;
+    this.selectedPrognosis = undefined;
+
+    if(value.statusId == this.defaultStatus.statusId){
+      this.isDisabledPrognosis = true;
+      this.dataResultPrognosis = [];
+    } else {
+      this.isDisabledPrognosis = false;
+      this.dataResultPrognosis = this.statusPrognosis.filter((s) => s.statusId === value.statusId)
+    }
+  }
+
+
+  /*private formGroup = new FormGroup({
+    incident_name: new FormControl(),
+    location: new FormControl(),
+    status: new FormControl([]),
+    prognosis: new FormControl([])
+  })
+
+
+  private fromArrayObjectToArrayOfOptions = (val : any[]) => (val.reduce((pre, elem) => [...pre, elem.options], []));
+  private fromArrayOfArraysToArray = (val) => (val.flat());
+
+  private subSource$ = this.formGroup.get('status').valueChanges.pipe(
+    map(this.fromArrayObjectToArrayOfOptions),
+    map(this.fromArrayOfArraysToArray));
+
+
+  private source$ = of([
+    {type: "Closed", options: [{prognosis: "Monitoring"}, {prognosis: "Response"}]},
+    {type: "Open", options: [{prognosis: "Monitoring"}, {prognosis: "Response"}, {prognosis: "Extended Operation"}]},
+    {type: "Special Attention", options: [{prognosis: "Monitoring"}]},
+  ]);*/
+
 
 
 
   //selectedValue: string = "";
-  statuses = [
+/* statuses = [
     {value: 'Open', viewValue: 'Open'},
     {value: 'Report Closed', viewValue: 'Report Closed'},
     {value: 'Special Attention', viewValue: 'Special Attention'}
@@ -45,7 +105,7 @@ export class HomeComponent implements OnInit {
 
   prognoses3 = [
     {value: 'Monitoring', viewValue: 'Monitoring'},
-  ]
+  ]*/
 
 
 
@@ -62,6 +122,7 @@ export class HomeComponent implements OnInit {
   }
 
   onSubmitIncident() {
+    this.model.STATUS = this.selectedStatus.statusName + ' ' + this.selectedPrognosis.prognosisName;
     this._incidentService.addIncidents(this.model).subscribe(
       newIncident => {
         this.ngOnInit();
@@ -72,7 +133,7 @@ export class HomeComponent implements OnInit {
     );
   }
 
-  onSelect(){
+    /*onSelect(){
     if(this.model.STATUS == "Open"){
       this.check1 = "true";
       this.check2 = "false";
@@ -88,7 +149,7 @@ export class HomeComponent implements OnInit {
       this.check2 = "false";
       this.check3 = "true";
     }
-  }
+  }*/
   incidentSelect(dataSource: IncidentsDataSource) {
     this.dataSource = dataSource;
   }
