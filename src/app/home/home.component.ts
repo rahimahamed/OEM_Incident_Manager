9f173942ff424f148aa3140a34157470d46eda45
Saha <rsaha@OEM.NYCNET>
2019-07-31 12:46:00 -0400

    Material From ngModel

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -21,70 +21,47 @@ export class HomeComponent implements OnInit {
   date: number = Date.now();
   dataSource: IncidentsDataSource;
 
-  /*public isDisabledPrognosis: boolean = true;
+  model = new Incident();
 
-  public defaultStatus: { statusName: string, statusId: number} = { statusName: "Select A Status", statusId: null};
-  public defaultPrognosis: { prognosisName: string, prognosisId: number} = { prognosisName: "Select a Prognosis", prognosisId: null};
+  //status;
+  prognosis;
+  prognosisList = [];
+  progTest;
 
-  public statusCategory: Array<{ statusName: string, statusId: number}> = [
-    {statusName: "Report Closed", statusId: 0},
-    {statusName: "Open", statusId: 1},
-    {statusName: "Special Attention", statusId: 2}
+  statusList: any = [
+    {
+      'statusName': 'Report Closed',
+      prognosisList: [
+        'Monitoring', 'Response',
+      ]
+    },
+    {
+      'statusName': 'Open',
+      prognosisList: [
+        'Monitoring', 'Response', 'Extended Operation',
+      ]
+    },
+    {
+      'statusName': 'Special Attention',
+      prognosisList: [
+        'Monitoring',
+      ]
+    }
   ];
 
-  public statusPrognosis: Array<{ prognosisName: string, prognosisId: number, statusId: number}> = [
-    {prognosisName: "Monitoring", prognosisId: 0, statusId: 0},
-    {prognosisName: "Response", prognosisId: 1, statusId: 0},
-    {prognosisName: "Monitoring", prognosisId: 2, statusId: 1},
-    {prognosisName: "Response", prognosisId: 3, statusId: 1},
-    {prognosisName: "Extended Operation", prognosisId: 4, statusId: 1},
-    {prognosisName: "Monitoring", prognosisId: 5, statusId: 2},
-  ]
-
-  public dataResultPrognosis: Array<{prognosisName: string, prognosisId: number, statusId: number}>;
-
-  public selectedStatus: {statusName: string, statusId: number};
-  public selectedPrognosis: {prognosisName: string, prognosisId: number};
-
-  handleStatusChange(value){
-    this.selectedStatus = value;
-    this.selectedPrognosis = undefined;
-    console.log(this.selectedStatus.statusName);
-
-    if(value.statusId == this.defaultStatus.statusId){
-      this.isDisabledPrognosis = true;
-      this.dataResultPrognosis = [];
+  statusChangeAction(statusTitle){
+    this.prognosis = "";
+    let dropDownData = this.prognosisList.find((data: any) => data.statusName === status)
+    if(dropDownData){
+      this.prognosisList = dropDownData.prognosisList;
+      if(this.prognosisList){
+        this.prognosis=this.prognosisList[0];
+      }
     } else {
-      this.isDisabledPrognosis = false;
-      this.dataResultPrognosis = this.statusPrognosis.filter((s) => s.statusId === value.statusId)
+      this.prognosisList = [];
     }
-  }*/
+  }
 
-
-  private formGroup = new FormGroup({
-    status: new FormControl([]),
-    prognosis: new FormControl([])
-  })
-
-
-  private fromArrayObjectToArrayOfOptions = (val : any[]) => (val.reduce((pre, elem) => [...pre, elem.options], []));
-  private fromArrayOfArraysToArray = (val) => (val.flat());
-
-  private subSource$ = this.formGroup.get('status').valueChanges.pipe(
-    map(this.fromArrayObjectToArrayOfOptions),
-    map(this.fromArrayOfArraysToArray));
-
-
-  private source$ = of([
-    {type: "Closed", options: [{prognosis: "Monitoring"}, {prognosis: "Response"}]},
-    {type: "Open", options: [{prognosis: "Monitoring"}, {prognosis: "Response"}, {prognosis: "Extended Operation"}]},
-    {type: "Special Attention", options: [{prognosis: "Monitoring"}]},
-  ]);
-
-
-
-
-  model = new Incident();
 
   constructor(private _incidentService: IncidentService, private router: Router, private route: ActivatedRoute) { }
 
@@ -99,6 +76,7 @@ export class HomeComponent implements OnInit {
 
   onSubmitIncident() {
     console.log('Hello this is a test');
+    console.log(this.progTest);
     this._incidentService.addIncidents(this.model).subscribe(
       newIncident => {
         this.ngOnInit();
